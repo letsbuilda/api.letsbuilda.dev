2d2c8f73106b1147e2983ea8ded8d1e83e7adb05
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-03-11 18:55:10 -0600

    Move random number generation to generators module
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/api/modules/generators.py
+++ src/api/modules/generators.py
@@ -1,20 +1,14 @@
 """Generation of things"""
 
 import uuid
+from random import randint
 from typing import Literal
 
 from fastapi import APIRouter
-
 # pylint: disable-next=no-name-in-module
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 router_generators = APIRouter(prefix="/generators", tags=["generators"])
-
-
-class UUIDs(BaseModel):
-    """Model to hold a list of UUIDs"""
-
-    uuids: list[str]
 
 
 class UUIDConfig(BaseModel):
@@ -22,6 +16,12 @@ class UUIDConfig(BaseModel):
 
     uuid_type: Literal[1, 4]
     quantity: int = Field(gt=0, default=1)
+
+
+class UUIDs(BaseModel):
+    """Model to hold a list of UUIDs"""
+
+    uuids: list[str]
 
 
 @router_generators.post("/uuids/")
@@ -35,3 +35,31 @@ async def bulk_uuids(config: UUIDConfig) -> UUIDs:
         raise ValueError(f"unsupported UUID type: {config.uuid_type}")
     uuids = [str(function()) for _ in range(config.quantity)]
     return UUIDs(uuids=uuids)
+
+
+class NumbersConfig(BaseModel):
+    """Model to hold configuration for number generation"""
+
+    lower_bound: int | None = Field(default=1)
+    upper_bound: int | None = Field(default=1)
+    quantity: int | None = Field(gt=0, default=1)
+
+    @validator('upper_bound')
+    def upper_bound_must_be_greater_than_lower_bound(cls, v, values, **kwargs):
+        if 'lower_bound' in values and v < values['lower_bound']:
+            raise ValueError('upper bound must be greater than lower bound')
+        return v
+
+
+class Numbers(BaseModel):
+    """Model to hold a list of numbers"""
+
+    numbers: list[int]
+    total: int
+
+
+@router_generators.post("/random-numbers/")
+async def random_numbers(config: NumbersConfig) -> Numbers:
+    """Generate bulk random numbers"""
+    numbers = [randint(config.lower_bound, config.upper_bound) for _ in range(config.quantity)]
+    return Numbers(numbers=numbers, total=sum(numbers))
--- src/api/server.py
+++ src/api/server.py
@@ -1,12 +1,10 @@
 """API server definition"""
 
 from os import getenv
-from random import randint
 
 import sentry_sdk
 from fastapi import APIRouter, FastAPI
 from imsosorry import uwuify
-
 # pylint: disable-next=no-name-in-module
 from pydantic import BaseModel
 
@@ -31,13 +29,6 @@ class TextModel(BaseModel):
     """Generic model for accepting arbitrary plain-text input"""
 
     text: str
-
-
-class Numbers(BaseModel):
-    """Model to hold a list of numbers"""
-
-    numbers: list[int]
-    total: int
 
 
 app = FastAPI(
@@ -69,13 +60,5 @@ async def uwuify_route(text: TextModel):
     return {"text": uwuify(text.text)}
 
 
-@router_fun.get("/random-numbers/{quantity}/{range_high}")
-async def random_numbers(quantity: int, range_high: int) -> Numbers:
-    """Generate bulk random numbers"""
-    numbers = [randint(1, range_high) for _ in range(quantity)]
-    return Numbers(numbers=numbers, total=sum(numbers))
-
-
 app.include_router(router_fun)
-
 app.include_router(router_generators)
